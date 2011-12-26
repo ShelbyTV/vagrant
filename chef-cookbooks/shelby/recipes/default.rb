2deec921b1703416ee2e6ad7bcaf5d1d46e9fe3e
Joshua Samberg <josh@shelby.tv>
2011-12-26 14:35:15 -0500

    Updated directory structure and packaged preliminary shelby box (still has host-only-network problems)

--- chef-cookbooks/shelby/recipes/default.rb
+++ chef-cookbooks/shelby/recipes/default.rb
@@ -15,9 +15,17 @@ package "libcurl4-gnutls-dev" do
 	action :install
 end
 
+# libsasl2-dev and gettext required by memcached gem
+package "libsasl2-dev" do
+	action :install
+end
+
+package "gettext" do
+	action :install
+end
+
 # invoke recipes to install all necessary components
-require_recipe "git"
-require_recipe "redis"
+require_recipe "redis::server"
 require_recipe "memcached"
 require_recipe "mongodb::10gen_repo"
 require_recipe "mongodb"
@@ -27,6 +35,4 @@ node['rvm']['user_installs'] = [
     'default_ruby'  => '1.9.2'
   }
 ]
-
 require_recipe "rvm::user"
-require_recipe "rvm::vagrant"
